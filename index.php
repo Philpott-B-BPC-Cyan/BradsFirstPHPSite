60a7517cb03047dbf7ec41f1034f40c465a9a6ad
Philpott-B-BPC-Cyan <32195086+Philpott-B-BPC-Cyan@users.noreply.github.com>
2017-10-06 09:55:26 +0100

    Upload PHP file.

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<HTML>
+<Head>
+<Title>
+
+This is a Title
+
+</Title>
+</Head>
+
+<Body>
+<h1>
+
+This is a Header.
+
+</h1>
+
+This is some text
+
+<? php
+
+echo "First time PHP.";
+
+?>
+
+
+</Body>
+</HTML>
