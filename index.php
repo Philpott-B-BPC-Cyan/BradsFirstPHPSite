76486a523f539151630c32d0a9a5583a91dd5379
Philpott-B-BPC-Cyan <32195086+Philpott-B-BPC-Cyan@users.noreply.github.com>
2017-10-06 10:23:54 +0100

    The Table has been added

--- index.php
+++ index.php
@@ -22,7 +22,23 @@ This is some text
 echo "First time PHP.";
 
 ?>
-
+<table>  
+  <tr>
+  <th>Server Scripts</th>    
+  <th>Client Scripts</th>      
+  </tr>  
+  
+  <tr>    
+  <td>Perl</td>    
+  <td> HTML</td>      
+  </tr> 
+  
+  <tr>      
+  <td>PHP</td>     
+  <td> CSS </td>        
+  </tr>
+</table>
+   
 
 </Body>
 </HTML>
